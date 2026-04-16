7a46ab980f698f415d2b1db926a10e948bcf097f
MuratDeveloper <230502007@kocaelisaglik.edu.tr>
2026-04-16 17:52:50 +0300

    Dialog Fix + New Dialog for the Trap

--- src/game/interaction.c
+++ src/game/interaction.c
@@ -1866,7 +1866,10 @@ void update_arch_delayed_items(struct MarioState *m) {
             set_mario_action(m, ACT_GETTING_BLOWN, 0);
             break;
         case SM64AP_ID_LITERATURE_TRAP:
-            set_mario_action(m, ACT_READING_AUTOMATIC_DIALOG, (random_u16() % 2 == 0) ? DIALOG_LITERATURE_TRAP : DIALOG_LITERATURE_TRAP_2);
+            {
+                u32 trapDialogs[] = { DIALOG_LITERATURE_TRAP, DIALOG_LITERATURE_TRAP_2, DIALOG_LITERATURE_TRAP_3 };
+                set_mario_action(m, ACT_READING_AUTOMATIC_DIALOG, trapDialogs[random_u16() % 3]);
+            }
             break;
         default:
             break;
