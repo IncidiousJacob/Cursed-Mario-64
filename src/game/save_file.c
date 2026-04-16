a51aec43998ce1de25c200d90be1fa6d511de417
MuratDeveloper <230502007@kocaelisaglik.edu.tr>
2026-04-16 19:09:40 +0300

    Double Checks for Bowser Keys

--- src/game/save_file.c
+++ src/game/save_file.c
@@ -511,6 +511,7 @@ void save_file_collect_star_or_key(s16 coinScore, s16 starIndex) {
                 save_file_set_flags(SAVE_FLAG_HAVE_KEY_1);
             }
             SM64AP_SendItem(SM64AP_ID_KEY1);
+            SM64AP_SendItem(SM64AP_ID_KEY1 + SM64AP_SECOND_CHECK_OFFSET);
             SM64AP_FinishBowser(0);
             break;
 
@@ -519,6 +520,7 @@ void save_file_collect_star_or_key(s16 coinScore, s16 starIndex) {
                 save_file_set_flags(SAVE_FLAG_HAVE_KEY_2);
             }
             SM64AP_SendItem(SM64AP_ID_KEY2);
+            SM64AP_SendItem(SM64AP_ID_KEY2 + SM64AP_SECOND_CHECK_OFFSET);
             SM64AP_FinishBowser(1);
             break;
 
