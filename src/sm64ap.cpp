2693c6e887363d3addf8787f41bebcb20fdd0928
MuratDeveloper <230502007@kocaelisaglik.edu.tr>
2026-04-18 15:58:09 +0300

    Enemysanity: CCM

--- src/sm64ap.cpp
+++ src/sm64ap.cpp
@@ -466,53 +466,65 @@ void SM64AP_SendItem(int idx) {
 }
 
 void SM64AP_CheckEnemyDeath(struct Object *o) {
-    if (gCurrLevelNum != LEVEL_BOB) return;
-
     int64_t loc_id = 0;
     int hX = (int)roundf(o->oHomeX);
     int hZ = (int)roundf(o->oHomeZ);
 
-    if (o->behavior == bhvGoomba) {
-        // Direct Macro Goombas
-        if (hX == -2713 && hZ == 5778) loc_id = 3626300;
-        else if (hX == -342 && hZ == 5433) loc_id = 3626301;
-        
-        // Triplet Spawned Goombas
-        else if (o->parentObj != o) {
-            int pHX = (int)roundf(o->parentObj->oPosX);
-            int pHZ = (int)roundf(o->parentObj->oPosZ);
-            int raw_idx = (o->oBehParams2ndByte & 0xFC); // GOOMBA_BP_TRIPLET_FLAG_MASK
-            int tri_idx = -1;
+    if (gCurrLevelNum == LEVEL_BOB) {
+        if (o->behavior == bhvGoomba) {
+            // Direct Macro Goombas
+            if (hX == -2713 && hZ == 5778) loc_id = 3626300;
+            else if (hX == -342 && hZ == 5433) loc_id = 3626301;
             
-            // Map common triplet flags to indices 0, 1, 2
-            // Typically 4, 8, 16 but bits might be shifted or combined with size
-            if (raw_idx & 0x04) tri_idx = 0;
-            else if (raw_idx & 0x08) tri_idx = 1;
-            else if (raw_idx & 0x10) tri_idx = 2;
-            
-            if (tri_idx != -1) {
-                if (pHX == 3640 && pHZ == 6280) loc_id = 3626302 + tri_idx;
-                else if (pHX == 6060 && pHZ == 2000) loc_id = 3626305 + tri_idx;
-                else if (pHX == -6050 && pHZ == 1250) loc_id = 3626308 + tri_idx;
+            // Triplet Spawned Goombas
+            else if (o->parentObj != o) {
+                int pHX = (int)roundf(o->parentObj->oPosX);
+                int pHZ = (int)roundf(o->parentObj->oPosZ);
+                int raw_idx = (o->oBehParams2ndByte & 0xFC); // GOOMBA_BP_TRIPLET_FLAG_MASK
+                int tri_idx = -1;
+                
+                // Map common triplet flags to indices 0, 1, 2
+                // Typically 4, 8, 16 but bits might be shifted or combined with size
+                if (raw_idx & 0x04) tri_idx = 0;
+                else if (raw_idx & 0x08) tri_idx = 1;
+                else if (raw_idx & 0x10) tri_idx = 2;
+                
+                if (tri_idx != -1) {
+                    if (pHX == 3640 && pHZ == 6280) loc_id = 3626302 + tri_idx;
+                    else if (pHX == 6060 && pHZ == 2000) loc_id = 3626305 + tri_idx;
+                    else if (pHX == -6050 && pHZ == 1250) loc_id = 3626308 + tri_idx;
+                }
             }
         }
-    }
-    else if (o->behavior == bhvBobomb) {
-        if (hX == -3080 && hZ == -5200) loc_id = 3626311;
-        else if (hX == -3688 && hZ == -3813) loc_id = 3626312;
-        else if (hX == -4629 && hZ == -1772) loc_id = 3626313;
-        else if (hX == -3480 && hZ == -2120) loc_id = 3626314;
-        else if (hX == -3800 && hZ == -460) loc_id = 3626315;
-        else if (hX == 6888 && hZ == -5608) loc_id = 3626316;
-        else if (hX == 2350 && hZ == 3700) loc_id = 3626317;
-        else if (hX == -1750 && hZ == -2800) loc_id = 3626318;
-        else if (hX == -1400 && hZ == -950) loc_id = 3626319;
-        else if (hX == -2650 && hZ == 1750) loc_id = 3626320;
-        else if (hX == -1900 && hZ == 3450) loc_id = 3626321;
-        else if (hX == 1127 && hZ == -2495) loc_id = 3626322;
-    }
-    else if (o->behavior == bhvKoopa) {
-        if (hX == 3400 && hZ == 6500) loc_id = 3626323;
+        else if (o->behavior == bhvBobomb) {
+            if (hX == -3080 && hZ == -5200) loc_id = 3626311;
+            else if (hX == -3688 && hZ == -3813) loc_id = 3626312;
+            else if (hX == -4629 && hZ == -1772) loc_id = 3626313;
+            else if (hX == -3480 && hZ == -2120) loc_id = 3626314;
+            else if (hX == -3800 && hZ == -460) loc_id = 3626315;
+            else if (hX == 6888 && hZ == -5608) loc_id = 3626316;
+            else if (hX == 2350 && hZ == 3700) loc_id = 3626317;
+            else if (hX == -1750 && hZ == -2800) loc_id = 3626318;
+            else if (hX == -1400 && hZ == -950) loc_id = 3626319;
+            else if (hX == -2650 && hZ == 1750) loc_id = 3626320;
+            else if (hX == -1900 && hZ == 3450) loc_id = 3626321;
+            else if (hX == 1127 && hZ == -2495) loc_id = 3626322;
+        }
+        else if (o->behavior == bhvKoopa) {
+            if (hX == 3400 && hZ == 6500) loc_id = 3626323;
+        }
+    } else if (gCurrLevelNum == LEVEL_CCM) {
+        if (o->behavior == bhvMrBlizzard) {
+            if (hX == -2376 && hZ == 4256) loc_id = 3626400;
+            else if (hX == -394 && hZ == 4878) loc_id = 3626401;
+            else if (hX == 3054 && hZ == 2072) loc_id = 3626402;
+        } else if (o->behavior == bhvSpindrift) {
+            if (hX == 2542 && hZ == -1714) loc_id = 3626403;
+            else if (hX == -6090 && hZ == 1936) loc_id = 3626404;
+            else if (hX == 4346 && hZ == 400) loc_id = 3626405;
+            else if (hX == -5054 && hZ == -1054) loc_id = 3626406;
+            else if (hX == -5033 && hZ == -2666) loc_id = 3626407;
+        }
     }
 
     if (loc_id != 0) {
