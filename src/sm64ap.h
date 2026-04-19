d19f65bdc54c566b912cbc5cf7cea4c2fbe77161
MuratDeveloper <230502007@kocaelisaglik.edu.tr>
2026-04-19 16:27:04 +0300

    Added logging

--- src/sm64ap.h
+++ src/sm64ap.h
@@ -140,6 +140,9 @@ AP_EXTERN_C void SM64AP_UpdateRRTrapTimer(struct MarioState *);
 // Print Next Message to Screen
 AP_EXTERN_C void SM64AP_PrintNext();
 
+// Logging
+AP_EXTERN_C void SM64AP_DebugLog(const char *fmt, ...);
+
 // If an item exists on the stack, return it, otherwise 0
 AP_EXTERN_C int64_t SM64AP_PopDelayedStack();
 
