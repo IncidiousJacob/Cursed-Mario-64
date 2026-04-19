04ffc122781bc6c597ff3af78a31234841945a76
Incidious <jacobdock9@gmail.com>
2026-04-18 19:43:08 -0600

    Refactor Piranha Plant behavior by removing scaling logic
    Removed conditional scaling and respawn logic for Piranha Plant.

--- src/game/behaviors/piranha_plant.inc.c
+++ src/game/behaviors/piranha_plant.inc.c
@@ -184,34 +184,6 @@ void piranha_plant_act_shrink_and_die(void) {
     piranha_plant_reset_when_far();
 }
 
-    /**
-     * Note that this if-statement occurs unconditionally after the above if-
-     * statement. Since the Piranha Plant's scale is 1.0f by default, perhaps
-     * this was intentional. However, it is equally plausible that the
-     * programmers meant to type `else if`.
-     */
-    if (o->oPiranhaPlantScale > 0.0f) {
-        // Shrink by 0.04 per frame.
-        o->oPiranhaPlantScale = o->oPiranhaPlantScale - 0.04;
-    } else {
-        o->oPiranhaPlantScale = 0.0f;
-        // Only spawn coin and change action if not already done in the first block
-        if (o->oAction != PIRANHA_PLANT_ACT_WAIT_TO_RESPAWN) {
-            cur_obj_spawn_loot_blue_coin();
-            o->oAction = PIRANHA_PLANT_ACT_WAIT_TO_RESPAWN;
-        }
-    }
-
-    cur_obj_scale(o->oPiranhaPlantScale);
-
-#if BUGFIX_PIRANHA_PLANT_STATE_RESET
-    piranha_plant_reset_when_far(); // see this function's comment
-#endif
-}
-
-/**
- * Wait for Mario to move far away, then respawn the Piranha Plant.
- */
 void piranha_plant_act_wait_to_respawn(void) {
     if (o->oDistanceToMario > 1200.0f) {
         o->oAction = PIRANHA_PLANT_ACT_RESPAWN;
