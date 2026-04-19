b50d5810f9f8fa60338fcda7289b5073a93c0fc7
Incidious <jacobdock9@gmail.com>
2026-04-18 18:35:56 -0600

    Simplify SM64AP_CheckWFPiranhaPlant function
    Refactor SM64AP_CheckWFPiranhaPlant to simplify logic and remove unnecessary checks.

--- src/sm64ap.cpp
+++ src/sm64ap.cpp
@@ -92,19 +92,8 @@ static void SM64AP_SpawnKoopaShellInFrontOfMario(void) {
 }
 
 void SM64AP_CheckWFPiranhaPlant(struct Object *o) {
-    if (gCurrLevelNum != LEVEL_WF || o == NULL) {
-        return;
-    }
-
-    int hX = (int)roundf(o->oHomeX);
-    int hZ = (int)roundf(o->oHomeZ);
-
-    if (hX == -1734 && hZ == -2457) {
-        if (!SM64AP_CheckedLoc(2400)) SM64AP_SendItem(2400);
-    } else if (hX == -3379 && hZ == -2457) {
-        if (!SM64AP_CheckedLoc(2401)) SM64AP_SendItem(2401);
-    } else if (hX == -2557 && hZ == -3876) {
-        if (!SM64AP_CheckedLoc(2402)) SM64AP_SendItem(2402);
+    if (gCurrLevelNum == LEVEL_WF) {
+        SM64AP_SendItem(2400);
     }
 }
 void SM64AP_RecvItem(int64_t idx, bool notify) {
