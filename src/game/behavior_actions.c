2bab2cdd6119c33801f692a269d705dcf09bada3
Incidious <jacobdock9@gmail.com>
2026-04-20 11:10:41 -0600

    Update behavior_actions.c

--- src/game/behavior_actions.c
+++ src/game/behavior_actions.c
@@ -280,7 +280,7 @@ s32 set_obj_anim_with_accel_and_sound(s16 a0, s16 a1, s32 a2) {
 #include "behaviors/castle_floor_trap.inc.c"
 #include "behaviors/pole_base.inc.c"
 #include "behaviors/sparkle_spawn.inc.c"
-#include "behaviors/scuttlebug.inc.c" // :scuttleeyes:
+#include "behaviors/Scuttlebug.inc.c" // :scuttleeyes:
 #include "behaviors/whomp.inc.c"
 #include "behaviors/water_splashes_and_waves.inc.c"
 #include "behaviors/strong_wind_particle.inc.c"
