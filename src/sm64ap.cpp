9aab6c5e3ab35c2789f119c64f899519a3a20482
Incidious <jacobdock9@gmail.com>
2026-04-20 07:48:42 -0600

    Update sm64ap.cpp

--- src/sm64ap.cpp
+++ src/sm64ap.cpp
@@ -187,30 +187,8 @@ void SM64AP_Boosanity(struct Object *o) {
 }
 
 void SM64AP_Scuttlesanity(struct Object *o) {
-    if (o == NULL || gCurrLevelNum != LEVEL_BBH) {
-        return;
-    }
-
-    int64_t loc_id = 0;
-
-    struct Object *src = o;
-
-    if (o->parentObj != NULL && o->parentObj != o) {
-        src = o->parentObj;
-    }
-
-    int x = (int) roundf(src->oPosX);
-    int z = (int) roundf(src->oPosZ);
-
-    if (x == -346 && z == -2813)
-        loc_id = 2600;
-    else if (x == 1146 && z == -2280)
-        loc_id = 2601;
-    else if (x == 3466 && z == 5106)
-        loc_id = 2602;
-
-    if (loc_id != 0 && !SM64AP_CheckedLoc(loc_id)) {
-        SM64AP_SendItem(loc_id);
+    if (!SM64AP_CheckedLoc(2600)) {
+        SM64AP_SendItem(2600);
     }
 }
 
