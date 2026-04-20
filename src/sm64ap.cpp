b2a457bf4c49ffd3896a8ec3bbd62b10b239c39d
Incidious <jacobdock9@gmail.com>
2026-04-19 19:33:35 -0600

    Add case for SM64AP_ID_BBBITDW_UNLOCK

--- src/sm64ap.cpp
+++ src/sm64ap.cpp
@@ -228,7 +228,7 @@ void SM64AP_RecvItem(int64_t idx, bool notify) {
                sm64_have_bitfs_bowser = true;
                break;
 
-           case SM64AP_ID_BSBITS_UNLOCK:
+           case SM64AP_ID_BBBITDW_UNLOCK:
               sm64_have_bitdw_bombs = true;
               break;
         }
