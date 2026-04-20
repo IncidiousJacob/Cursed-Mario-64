12bfbb26cf0d162104b84f628c6a626fe75566b5
Incidious <jacobdock9@gmail.com>
2026-04-20 08:43:36 -0600

    Handle interaction status for Scuttlebug behavior

--- src/game/behaviors/scuttlebug.inc.c
+++ src/game/behaviors/scuttlebug.inc.c
@@ -32,10 +32,15 @@ void bhv_scuttlebug_loop(void) {
 
     cur_obj_update_floor_and_walls();
 
+    if (o->oInteractStatus & INT_STATUS_WAS_ATTACKED) {
+        SM64AP_Scuttlesanity(o);
+    }
+
     if (o->oSubAction != 0
         && cur_obj_set_hitbox_and_die_if_attacked(&sScuttlebugHitbox, SOUND_OBJ_DYING_ENEMY1,
-                                                  o->oScuttlebugUnkF4))
+                                                  o->oScuttlebugUnkF4)) {
         o->oSubAction = 3;
+    }
 
     if (o->oSubAction != 1)
         o->oScuttlebugUnkF8 = 0;
@@ -87,7 +92,6 @@ void bhv_scuttlebug_loop(void) {
             break;
 
         case 3:
-            SM64AP_Scuttlesanity(o);
             o->oFlags &= ~8;
             o->oForwardVel = -10.0f;
             o->oVelY = 30.0f;
