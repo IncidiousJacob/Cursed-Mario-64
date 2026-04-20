37c3e0b5e616120ca9452c3cf1f02f37085d1efa
Incidious <jacobdock9@gmail.com>
2026-04-19 19:20:16 -0600

    Update sm64ap.h

--- src/sm64ap.h
+++ src/sm64ap.h
@@ -142,6 +142,7 @@ AP_EXTERN_C bool sm64_have_toad_136;
 AP_EXTERN_C bool sm64_have_bitdw_bowser;
 AP_EXTERN_C bool sm64_have_bitfs_bowser;
 AP_EXTERN_C bool sm64_have_bits_bowser;
+AP_EXTERN_C bool sm64_have_bitdw_bombs;
 AP_EXTERN_C void SM64AP_CheckWFPiranhaPlant(struct Object *);
 
 // Send Item
