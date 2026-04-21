af659d214e5b7d2b896287f9bd610d93b20a0ca1
Incidious <jacobdock9@gmail.com>
2026-04-21 08:52:25 -0600

    Adjust Piranha Plant colors in sm64ap.cpp

--- src/sm64ap.cpp
+++ src/sm64ap.cpp
@@ -144,9 +144,9 @@ void SM64AP_SetMarioPaletteSeed(int seed) {
 
     gGoombaColor = sm64ap_make_color(x, 96, 255);
 
-    gPiranhaHeadColor = sm64ap_make_color(x, 75, 255);
+    gPiranhaHeadColor = sm64ap_make_color(x, 60, 255);
     gPiranhaStemColor = sm64ap_make_color(x, 75, 255);
-    gPiranhaLeafColor = sm64ap_make_color(x, 75, 255);
+    gPiranhaLeafColor = sm64ap_make_color(x, 85, 255);
 
     SM64AP_ApplyMarioPalette();
     SM64AP_ApplyStarPalette();
