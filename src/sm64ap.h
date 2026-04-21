3e67fcda7436334587b38962ce577daf04eb1508
Incidious <jacobdock9@gmail.com>
2026-04-21 14:38:00 -0600

    Add Bobomb color and palette functions

--- src/sm64ap.h
+++ src/sm64ap.h
@@ -46,6 +46,9 @@ AP_EXTERN_C SM64AP_RGB8 gBowserBodyColor;
 
 AP_EXTERN_C void SM64AP_ApplyBowserPalette(void);
 
+AP_EXTERN_C SM64AP_RGB8 gBobombColor;
+AP_EXTERN_C void SM64AP_ApplyBobombPalette(void);
+
 #define SM64AP_ID_OFFSET 3626000
 #define SM64AP_SECOND_CHECK_OFFSET 1000
 #define SM64AP_ITEMID_STAR SM64AP_ID_OFFSET
