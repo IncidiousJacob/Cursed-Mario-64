7feaf8b2f30a1f131d5fbd07b905546cbec445b8
Incidious <jacobdock9@gmail.com>
2026-04-21 14:05:38 -0600

    Add Bowser color definitions and apply palette function
    Added Bowser's horn, belly, and hair color definitions, and a new function to apply Bowser's palette.

--- src/sm64ap.h
+++ src/sm64ap.h
@@ -43,9 +43,12 @@ AP_EXTERN_C SM64AP_RGB8 gPiranhaLeafColor;
 AP_EXTERN_C void SM64AP_ApplyPiranhaPalette(void);
 
 AP_EXTERN_C SM64AP_RGB8 gBowserBodyColor;
-AP_EXTERN_C SM64AP_RGB8 gBowserBellyColor;
-AP_EXTERN_C SM64AP_RGB8 gBowserHairColor;
 AP_EXTERN_C SM64AP_RGB8 gBowserShellColor;
+AP_EXTERN_C SM64AP_RGB8 gBowserHornColor;
+AP_EXTERN_C SM64AP_RGB8 gBowserHairColor;
+AP_EXTERN_C SM64AP_RGB8 gBowserBellyColor;
+
+AP_EXTERN_C void SM64AP_ApplyBowserPalette(void);
 
 #define SM64AP_ID_OFFSET 3626000
 #define SM64AP_SECOND_CHECK_OFFSET 1000
