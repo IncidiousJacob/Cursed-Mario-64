33d82d6b0bac25c1831721327bdf42b0dd401a48
Incidious <jacobdock9@gmail.com>
2026-04-23 08:20:00 -0600

    Change gStarColor color values

--- src/sm64ap.cpp
+++ src/sm64ap.cpp
@@ -170,7 +170,7 @@ void SM64AP_SetMarioPaletteSeed(int seed) {
     gMarioOverallsColor = sm64ap_make_color(x, 64, 255);
     gMarioShoesColor    = sm64ap_make_color(x, 70, 255);
     gMarioGlovesColor   = sm64ap_make_color(x, 75, 255);
-    gStarColor          = sm64ap_make_color(x, 69, 255);
+    gStarColor          = sm64ap_make_color(x, 77, 240);
 
     gToadBodyColor = sm64ap_make_color(x, 48, 255);
     gToadSpotColor = sm64ap_make_color(x, 96, 255);
