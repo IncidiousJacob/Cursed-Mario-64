a472a1830cf45b33681fe384ada783da2f56fb03
Incidious <jacobdock9@gmail.com>
2026-04-24 20:46:46 -0600

    Add Spindrift color definitions and palette function

--- src/sm64ap.h
+++ src/sm64ap.h
@@ -61,6 +61,10 @@ AP_EXTERN_C void SM64AP_ApplyBooPalette(void);
 AP_EXTERN_C SM64AP_RGB8 gBowserFlameColor;
 
 AP_EXTERN_C void SM64AP_ApplyBowserFlamePalette(void);
+AP_EXTERN_C SM64AP_RGB8 gSpindriftFlowerColor;
+AP_EXTERN_C SM64AP_RGB8 gSpindriftBodyColor;
+
+AP_EXTERN_C void SM64AP_ApplySpindriftPalette(void);
 
 #define SM64AP_ID_OFFSET 3626000
 #define SM64AP_SECOND_CHECK_OFFSET 1000
