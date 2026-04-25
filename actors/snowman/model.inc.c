6b14586ed52de095885474d5a238a5fe7f611edd
Incidious <jacobdock9@gmail.com>
2026-04-25 16:18:56 -0600

    Add function to set Mr. Blizzard light group

--- actors/snowman/model.inc.c
+++ actors/snowman/model.inc.c
@@ -200,7 +200,17 @@ tic void sm64ap_set_mr_blizzard_light_group(Lights1 *dst, u8 r, u8 g, u8 b) {
     dst->l[0].l.col[2] = b;
 }
 
-void SM64AP_ApplyMrBlizzardPalette(void) {
+static void sm64ap_set_mr_blizzard_light_group(Lights1 *dst, u8 r, u8 g, u8 b) {
+    dst->a.l.col[0] = r / 2;
+    dst->a.l.col[1] = g / 2;
+    dst->a.l.col[2] = b / 2;
+
+    dst->l[0].l.col[0] = r;
+    dst->l[0].l.col[1] = g;
+    dst->l[0].l.col[2] = b;
+}
+
+extern "C" void SM64AP_ApplyMrBlizzardPalette(void) {
     sm64ap_set_mr_blizzard_light_group(&snowman_seg5_lights_0500C470,
         gMrBlizzardColor.r,
         gMrBlizzardColor.g,
@@ -211,6 +221,8 @@ void SM64AP_ApplyMrBlizzardPalette(void) {
         gMrBlizzardMittenColor.g,
         gMrBlizzardMittenColor.b);
 }
+
+
 
 // 0x0500C7E8
 static const Vtx snowman_seg5_vertex_0500C7E8[] = {
