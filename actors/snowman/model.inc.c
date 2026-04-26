ef6c3b96c33ab70fc82777bac9ecedadfa7c49a4
Incidious <jacobdock9@gmail.com>
2026-04-26 11:03:55 -0600

    Delete Mr. Blizzard light group functions
    Removed the light group setup functions for Mr. Blizzard.

--- actors/snowman/model.inc.c
+++ actors/snowman/model.inc.c
@@ -190,28 +190,6 @@ static const Lights1 snowman_seg5_lights_0500C7D0 = gdSPDefLights1(
     0xff, 0xff, 0xff, 0x28, 0x28, 0x28
 );
 
-static void sm64ap_set_mr_blizzard_light_group(Lights1 *dst, u8 r, u8 g, u8 b) {
-    dst->a.l.col[0] = r / 2;
-    dst->a.l.col[1] = g / 2;
-    dst->a.l.col[2] = b / 2;
-
-    dst->l[0].l.col[0] = r;
-    dst->l[0].l.col[1] = g;
-    dst->l[0].l.col[2] = b;
-}
-
-void SM64AP_ApplyMrBlizzardPalette(void) {
-    sm64ap_set_mr_blizzard_light_group(&snowman_seg5_lights_0500C470,
-        gMrBlizzardColor.r,
-        gMrBlizzardColor.g,
-        gMrBlizzardColor.b);
-
-    sm64ap_set_mr_blizzard_light_group(&snowman_seg5_lights_0500C7D0,
-        gMrBlizzardMittenColor.r,
-        gMrBlizzardMittenColor.g,
-        gMrBlizzardMittenColor.b);
-}
-
 
 // 0x0500C7E8
 static const Vtx snowman_seg5_vertex_0500C7E8[] = {
