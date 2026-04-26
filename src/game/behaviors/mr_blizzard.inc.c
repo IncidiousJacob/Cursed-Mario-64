08819ae43e6246fb9d7300687f1117caaab77807
Incidious <jacobdock9@gmail.com>
2026-04-26 08:22:07 -0600

    Add palette application for Mr. Blizzard initialization

--- src/game/behaviors/mr_blizzard.inc.c
+++ src/game/behaviors/mr_blizzard.inc.c
@@ -42,6 +42,8 @@ void mr_blizzard_spawn_white_particles(s8 count, s8 offsetY, s8 forwardVelBase, 
  */
 
 void bhv_mr_blizzard_init(void) {
+    SM64AP_ApplyMrBlizzardPalette();
+
     if (o->oBehParams2ndByte == MR_BLIZZARD_STYPE_JUMPING) {
         // Jumping Mr. Blizzard.
         o->oAction = MR_BLIZZARD_ACT_JUMP;
