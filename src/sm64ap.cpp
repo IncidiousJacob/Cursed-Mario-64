67f25c0a1a69d3cd05c9a101727fef973b37f71f
Incidious <jacobdock9@gmail.com>
2026-04-27 11:10:04 -0600

    Add Boo color and apply Boo palette

--- src/sm64ap.cpp
+++ src/sm64ap.cpp
@@ -108,6 +108,7 @@ SM64AP_RGB8 gBobombMetalColor;
 SM64AP_RGB8 gPenguinBodyColor;
 SM64AP_RGB8 gPenguinBellyColor;
 SM64AP_RGB8 gPenguinBeakColor;
+SM64AP_RGB8 gBooColor;
 
 int sm64_ap_health_items_received = 0;
 
@@ -220,6 +221,7 @@ void SM64AP_SetMarioPaletteSeed(int seed) {
     gPenguinBodyColor = sm64ap_make_color(x, 64, 255);
     gPenguinBellyColor = sm64ap_make_color(x, 96, 255);
     gPenguinBeakColor = sm64ap_make_color(x, 80, 255);
+    gBooColor = sm64ap_make_color(x, 75, 255);
 
     SM64AP_ApplyMarioPalette();
     SM64AP_ApplyStarPalette();
@@ -229,6 +231,7 @@ void SM64AP_SetMarioPaletteSeed(int seed) {
     SM64AP_ApplyBowserPalette();
     SM64AP_ApplyBobombPalette();
     SM64AP_ApplyPenguinPalette();
+    SM64AP_ApplyBooPalette();
 }
 
 
