286e39f5bce99b6006bd9218bfb51881661de09f
Incidious <jacobdock9@gmail.com>
2026-04-30 17:52:03 -0600

    Fix condition for sending item in Mario cutscene

--- src/game/mario_actions_cutscene.c
+++ src/game/mario_actions_cutscene.c
@@ -575,7 +575,7 @@ s32 act_reading_sign(struct MarioState *m) {
                  if (dialogID == DIALOG_102) {
                     if (gCurrLevelNum == LEVEL_COURTYARD && !SM64AP_CheckedLoc(5511)) {
                         SM64AP_SendItem(5511);
-                    } else if (gCurrLevelNum == LEVEL_ && !SM64AP_CheckedLoc(5682)) {
+                    } else if (gCurrLevelNum == LEVEL_BBH && !SM64AP_CheckedLoc(5682)) {
                         SM64AP_SendItem(5682);
                     }
                  }
