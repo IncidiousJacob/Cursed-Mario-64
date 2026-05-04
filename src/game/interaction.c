568fe1d39329e9b6f547b78727473c1fbe363de2
Incidious <jacobdock9@gmail.com>
2026-05-04 13:41:27 -0600

    Add debug variables for coin interaction
    Added debug variables for coin interaction tracking.

--- src/game/interaction.c
+++ src/game/interaction.c
@@ -1,5 +1,6 @@
 #include <PR/ultratypes.h>
 
+#include <stdio.h> // if not already included
 #include "../sm64ap.h"
 #include "area.h"
 #include "actors/common1.h"
@@ -77,6 +78,10 @@ u32 interact_hoot(struct MarioState *, u32, struct Object *);
 u32 interact_cap(struct MarioState *, u32, struct Object *);
 u32 interact_grabbable(struct MarioState *, u32, struct Object *);
 u32 interact_text(struct MarioState *, u32, struct Object *);
+s16 gCoinDebugTimer = 0;
+s32 gCoinDebugX = 0;
+s32 gCoinDebugY = 0;
+s32 gCoinDebugZ = 0;
 
 u16 delayedArchTimer = 100;
 
@@ -742,6 +747,13 @@ void reset_mario_pitch(struct MarioState *m) {
 }
 
 u32 interact_coin(struct MarioState *m, UNUSED u32 interactType, struct Object *o) {
+
+    // --- DEBUG ---
+    gCoinDebugX = (s32)o->oPosX;
+    gCoinDebugY = (s32)o->oPosY;
+    gCoinDebugZ = (s32)o->oPosZ;
+    gCoinDebugTimer = 180;
+
     m->numCoins += o->oDamageOrCoinValue;
     m->healCounter += 4 * o->oDamageOrCoinValue;
 
@@ -751,7 +763,7 @@ u32 interact_coin(struct MarioState *m, UNUSED u32 interactType, struct Object *
         && m->numCoins >= 100) {
         bhv_spawn_star_no_level_exit(6);
     }
-   
+
     if (o->oDamageOrCoinValue >= 2) {
         queue_rumble_data(5, 80);
     }
