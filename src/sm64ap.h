8261785a8fe145be13a4026c78fe55b684e593ed
Incidious <jacobdock9@gmail.com>
2026-05-05 12:00:05 -0600

    Add function to update level coin count

--- src/sm64ap.h
+++ src/sm64ap.h
@@ -208,6 +208,7 @@ AP_EXTERN_C int SM64AP_GetLockedCoinLoc(struct Object *o);
 AP_EXTERN_C void SM64AP_CheckLockedCoin(struct Object *o);
 AP_EXTERN_C void SM64AP_AddCoinsToCourse(s16 course, s16 amount);
 AP_EXTERN_C void SM64AP_HandleLevelCoinItem(s32 itemId);
+AP_EXTERN_C void SM64AP_UpdateLevelCoinCount(void);
 
 // Send Item
 AP_EXTERN_C void SM64AP_SendByBoxID(int);
