5b4ee7aa491dbee36d111f46131fbf014e9f144e
Incidious <jacobdock9@gmail.com>
2026-05-05 09:30:58 -0600

    Refactor reset function for coinsanity flags
    Refactor SM64AP_ResetCoinsanityFlags to use a loop for resetting flags.

--- src/sm64ap_coinsanity.h
+++ src/sm64ap_coinsanity.h
@@ -3,6 +3,9 @@
 
 // Coinsanity locked coin item received flags.
 // Add future locked coin bools here.
+//
+// IMPORTANT:
+// This header defines real bool variables, so only include it from sm64ap.cpp.
 
 bool sm64_have_locked_coin_6000 = false;
 bool sm64_have_locked_coin_6001 = false;
@@ -14,16 +17,52 @@ bool sm64_have_locked_coin_6006 = false;
 bool sm64_have_locked_coin_6007 = false;
 bool sm64_have_locked_coin_6008 = false;
 
+typedef struct {
+    int itemId;
+    bool *flag;
+} SM64APCoinsanityReturnItemEntry;
+
+// This is the return/received item table.
+// When AP returns item 6000, it sets sm64_have_locked_coin_6000 = true.
+// When AP returns item 6001, it sets sm64_have_locked_coin_6001 = true.
+// etc.
+static SM64APCoinsanityReturnItemEntry sCoinsanityReturnItemTable[] = {
+    { 6000, &sm64_have_locked_coin_6000 },
+    { 6001, &sm64_have_locked_coin_6001 },
+    { 6002, &sm64_have_locked_coin_6002 },
+    { 6003, &sm64_have_locked_coin_6003 },
+    { 6004, &sm64_have_locked_coin_6004 },
+    { 6005, &sm64_have_locked_coin_6005 },
+    { 6006, &sm64_have_locked_coin_6006 },
+    { 6007, &sm64_have_locked_coin_6007 },
+    { 6008, &sm64_have_locked_coin_6008 },
+};
+
+static bool SM64AP_SetCoinsanityReturnItemFlag(int64_t idx) {
+    for (int i = 0; i < (int)(sizeof(sCoinsanityReturnItemTable) / sizeof(sCoinsanityReturnItemTable[0])); i++) {
+        if (idx == sCoinsanityReturnItemTable[i].itemId) {
+            *sCoinsanityReturnItemTable[i].flag = true;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+static bool SM64AP_HaveCoinsanityReturnItemFlag(int itemId) {
+    for (int i = 0; i < (int)(sizeof(sCoinsanityReturnItemTable) / sizeof(sCoinsanityReturnItemTable[0])); i++) {
+        if (itemId == sCoinsanityReturnItemTable[i].itemId) {
+            return *sCoinsanityReturnItemTable[i].flag;
+        }
+    }
+
+    return false;
+}
+
 static void SM64AP_ResetCoinsanityFlags(void) {
-    sm64_have_locked_coin_6000 = false;
-    sm64_have_locked_coin_6001 = false;
-    sm64_have_locked_coin_6002 = false;
-    sm64_have_locked_coin_6003 = false;
-    sm64_have_locked_coin_6004 = false;
-    sm64_have_locked_coin_6005 = false;
-    sm64_have_locked_coin_6006 = false;
-    sm64_have_locked_coin_6007 = false;
-    sm64_have_locked_coin_6008 = false;
+    for (int i = 0; i < (int)(sizeof(sCoinsanityReturnItemTable) / sizeof(sCoinsanityReturnItemTable[0])); i++) {
+        *sCoinsanityReturnItemTable[i].flag = false;
+    }
 }
 
 #endif
