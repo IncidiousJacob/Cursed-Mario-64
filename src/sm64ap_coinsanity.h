311f0baf769afcecb62b075ebdddaf321c4c93ec
Incidious <jacobdock9@gmail.com>
2026-05-05 06:25:20 -0600

    Add additional locked coin flags and reset logic

--- src/sm64ap_coinsanity.h
+++ src/sm64ap_coinsanity.h
@@ -8,12 +8,22 @@ bool sm64_have_locked_coin_6000 = false;
 bool sm64_have_locked_coin_6001 = false;
 bool sm64_have_locked_coin_6002 = false;
 bool sm64_have_locked_coin_6003 = false;
+bool sm64_have_locked_coin_6004 = false;
+bool sm64_have_locked_coin_6005 = false;
+bool sm64_have_locked_coin_6006 = false;
+bool sm64_have_locked_coin_6007 = false;
+bool sm64_have_locked_coin_6008 = false;
 
 static void SM64AP_ResetCoinsanityFlags(void) {
     sm64_have_locked_coin_6000 = false;
     sm64_have_locked_coin_6001 = false;
     sm64_have_locked_coin_6002 = false;
     sm64_have_locked_coin_6003 = false;
+    sm64_have_locked_coin_6004 = false;
+    sm64_have_locked_coin_6005 = false;
+    sm64_have_locked_coin_6006 = false;
+    sm64_have_locked_coin_6007 = false;
+    sm64_have_locked_coin_6008 = false;
 }
 
 #endif
