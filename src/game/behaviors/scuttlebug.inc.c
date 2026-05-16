65a781cefaece3d880f1ad496f52d8e81fa8040f
OnlyLeafeon <jeddytothelevvy@gmail.com>
2026-05-16 09:41:23 -0600

    Refactor Scuttlebug behavior on attack

--- src/game/behaviors/scuttlebug.inc.c
+++ src/game/behaviors/scuttlebug.inc.c
@@ -32,17 +32,20 @@ void bhv_scuttlebug_loop(void) {
 
     cur_obj_update_floor_and_walls();
 
-if (cur_obj_set_hitbox_and_die_if_attacked(&sScuttlebugHitbox,
-	SOUND_OBJ_DYING_ENEMY1, o->oScuttlebugUnkF4)) {
+	int bp2 = o->oBehParams2ndByte;
+	float hx = o->oHomeX;
+	float hz = o->oHomeZ;
 
-	SM64AP_SendScuttlebug(
-		o->oBehParams2ndByte,
-		o->oHomeX,
-		o->oHomeZ
-	);
+	if (cur_obj_set_hitbox_and_die_if_attacked(&sScuttlebugHitbox,
+		SOUND_OBJ_DYING_ENEMY1, o->oScuttlebugUnkF4)) {
 
-	o->oSubAction = 3;
-}
+		o->oBehParams2ndByte = bp2;
+		o->oHomeX = hx;
+		o->oHomeZ = hz;
+
+		SM64AP_Scuttlesanity(o);
+		o->oSubAction = 3;
+	}
 
     if (o->oSubAction != 1)
         o->oScuttlebugUnkF8 = 0;
